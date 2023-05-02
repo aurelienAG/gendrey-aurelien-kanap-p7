5d30f0c86521e2ad5753943a45c18d000562dd4e
Aurélien Gendrey <aurelien@MacBook-Air-de-Aurelien.local>
2023-05-03 01:07:57 +0200

    ajout des styles et création du header

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import logoKasa from "../assets/images/logo.png";
+function Header(){
+    return (
+    <header class="d-flex">
+        <img src={logoKasa}
+        alt="logo kasa"/> 
+        <nav>
+        <ul class="d-flex">
+          <li>Accueil</li>
+          <li>A propos</li>
+        </ul>
+        </nav>
+  </header>
+    );  
+  }
+    export default Header;

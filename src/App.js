6f710c4667fb8e25ecf109687743dd4b9e1f2dde
Aurélien Gendrey <aurelien@MacBook-Air-de-Aurelien.local>
2023-05-08 19:07:25 +0200

    design des cards

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage'
-// import About from './pages/About/About'
-// import Lodgingpage from './pages/Lodgingpage/Lodgingpage'
-// import Error from './pages/Error/Error'
+import About from './pages/About/About'
+import Lodgingpage from './pages/Lodgingpage/Lodgingpage'
+import Error from './pages/Error/Error'
 
 
 function App() {
@@ -10,9 +10,9 @@ function App() {
     <div className="pageBloc">
     <Routes>
       <Route path="/" element = {<Homepage />}/> 
-      {/* <Route path="/about" element = { <About />}/> 
+      <Route path="/about" element = { <About />}/> 
       <Route path="/lodging/:id" element = {<Lodgingpage/>}/> 
-      <Route path="*" element = {<Error/>} /> */}
+      <Route path="*" element = {<Error/>} />
     </Routes>
       </div>
   );
